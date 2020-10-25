0b6436f583a974c4e3fcd26b9b7977821a31d9aa
hegrace0101 <60679526+hegrace0101@users.noreply.github.com>
2020-10-24 21:20:09 -0400

    adding titleScreen changes

--- divhacks/src/Screens/TitleScreen.java
+++ divhacks/src/Screens/TitleScreen.java
@@ -15,41 +15,40 @@ public class TitleScreen extends Screen implements ActionListener {
 	private AllScreen as;
 	//private int t;
 	
-	//private Image nyanCat;
+	private Image runner1;
+	private Image runner2;
+	private Image runner3;
 	private int xPos, yPos;
 
 	public TitleScreen(AllScreen as) {
 		this.as = as;
-		//titleImg = (new ImageIcon("FlingTitleScreen.png").getImage());
-		//nyanCat = (new ImageIcon("poptart.gif").getImage());
-		
-		//t = 0;
-		//Timer clock = new Timer(30, this);
-		//clock.start();
-		//xPos = -200;
-		//yPos = 75;
+		runner1 = (new ImageIcon("divhacks/Runner1.png").getImage());
+		runner2 = (new ImageIcon("divhacks/Runner2.png").getImage());
+		runner3 = (new ImageIcon("divhacks/Runner3.png").getImage());
 	}
 
 	public void paintComponent(Graphics g) {
 
 		Graphics2D g2 = (Graphics2D) g;
 
-		//g.drawImage(titleImg, 0, 0, 800, 600, this);
+		g.drawImage(runner1, 530, 100, 289, 398, this);
+		g.drawImage(runner2, 290, 110, 273, 380, this);
+		g.drawImage(runner3, 25, 110, 285, 375, this);
 		
 		//if (xPos > 1000) {
 			//xPos = -200;
 		//}
 		
 		//g.drawImage(nyanCat, xPos, yPos, 200, 150, this);
-
+		g.setColor(new Color(255,255,255));
 		Font newFont = new Font("Arial", Font.BOLD, 20);
 		g.setFont(newFont);
-		g.drawString("The Social Distancing Game", 185, 50);
+		g.drawString("The Social Distancing Game", 250, 50);
 		
-		g.setColor(new Color(192,192,19));
-		g2.fillRoundRect(180, 330, 100, 50, 30, 30);
-		g.setColor(new Color(0,0,0));
-		g.drawString("Start!",180, 330);
+		g.setColor(new Color(192,192,192));
+		g2.fillRoundRect(350, 430, 100, 50, 30, 30);
+		g.setColor(new Color(255,255,255));
+		g.drawString("Start!", 370, 460);
 
 	}
 
@@ -58,7 +57,7 @@ public class TitleScreen extends Screen implements ActionListener {
 		int xClick = e.getX();
 		int yClick = e.getY();
 
-		if(xClick >= 180 && xClick <= 280 && yClick >= 330 && yClick <= 380) {
+		if(xClick >= 350 && xClick <= 450 && yClick >= 430 && yClick <= 480) {
 			as.changeScreen("Results"); // change later
 		}
 	}
