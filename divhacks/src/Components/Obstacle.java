91b73d25f88ac27b1ea1a91bba0056e07a6edb90
Jannie <jz3191@columbia.edu>
2020-10-24 17:10:20 -0700

    created obstacle class

--- divhacks/src/Components/Obstacle.java
+++ divhacks/src/Components/Obstacle.java
@@ -1,7 +1,45 @@
 package Components;
 
+import javax.swing.*;
+import java.awt.Image;
+
 public class Obstacle {
-    int xPos;
-    int yPos;
-    boolean hasMask;
+    private int xPos;
+    private int yPos;
+    private Image img;
+    private boolean hasMask;
+    private int size = 10; // idk we'll figure this out
+
+
+    public Obstacle(int x, int y, boolean hasMask){
+        xPos = x;
+        yPos = y;
+
+        this.hasMask = hasMask;
+        if(hasMask){
+            img = (new ImageIcon("shelbyface.png").getImage());
+        } else {
+            img = (new ImageIcon("shelbyface.png").getImage());
+        }
+
+    }
+    public int getX(){
+        return xPos;
+    }
+
+    public int getY(){
+        return yPos;
+    }
+
+
+    public void moveY (int y){
+        yPos += y;
+    }
+
+
+    public boolean hasMask(){
+        return hasMask;
+    }
+
+
 }
