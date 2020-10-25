950e9320c988f40ea1815e9d6deac9dede1bf2b9
Jannie <jz3191@columbia.edu>
2020-10-24 20:45:42 -0700

    all obstacle pictures added

--- divhacks/src/Components/Obstacle.java
+++ divhacks/src/Components/Obstacle.java
@@ -13,9 +13,25 @@ public class Obstacle {
     private boolean hasMask;
     private int size = 100;
 
-    private String[] nomaskPaths = {"/divhacks/assets/obstacles/nomask/black_nomask.png"};
+    private String[] noMaskPaths = {"/divhacks/assets/obstacles/nomask/blue_nomask.png", "/divhacks/assets/obstacles/nomask/blue_nomask_2.png",
+        "/divhacks/assets/obstacles/nomask/cyan_nomask.png", "/divhacks/assets/obstacles/nomask/cyan_nomask_2.png",
+        "/divhacks/assets/obstacles/nomask/green_nomask.png", "/divhacks/assets/obstacles/nomask/green_nomask_2.png",
+        "/divhacks/assets/obstacles/nomask/lime_nomask.png", "/divhacks/assets/obstacles/nomask/lime_nomask_2.png",
+        "/divhacks/assets/obstacles/nomask/orange_nomask.png", "/divhacks/assets/obstacles/nomask/orange_nomask_2.png",
+        "/divhacks/assets/obstacles/nomask/pink_nomask.png", "/divhacks/assets/obstacles/nomask/pink_nomask_2.png",
+        "/divhacks/assets/obstacles/nomask/red_nomask.png", "/divhacks/assets/obstacles/nomask/red_nomask_2.png",
+        "/divhacks/assets/obstacles/nomask/white_nomask.png", "/divhacks/assets/obstacles/nomask/white_nomask_2.png"
+            };
 
-
+    private String[] maskPaths = {"/divhacks/assets/obstacles/mask/blue_mask.png", "/divhacks/assets/obstacles/mask/blue_mask_2.png",
+        "/divhacks/assets/obstacles/mask/cyan_mask.png", "/divhacks/assets/obstacles/mask/cyan_mask_2.png",
+        "/divhacks/assets/obstacles/mask/green_mask.png", "/divhacks/assets/obstacles/mask/green_mask_2.png",
+        "/divhacks/assets/obstacles/mask/lime_mask.png", "/divhacks/assets/obstacles/mask/lime_mask_2.png",
+        "/divhacks/assets/obstacles/mask/orange_mask.png", "/divhacks/assets/obstacles/mask/orange_mask_2.png",
+        "/divhacks/assets/obstacles/mask/pink_mask.png", "/divhacks/assets/obstacles/mask/pink_mask_2.png",
+        "/divhacks/assets/obstacles/mask/red_mask.png", "/divhacks/assets/obstacles/mask/red_mask_2.png",
+        "/divhacks/assets/obstacles/mask/white_mask.png", "/divhacks/assets/obstacles/mask/white_mask_2.png"
+            };
 
 
     public Obstacle(int x, int y, boolean hasMask){
@@ -24,7 +40,7 @@ public class Obstacle {
 
         this.hasMask = hasMask;
         if(hasMask){
-            img = (new ImageIcon(chooseNoMaskImg()).getImage());
+            img = (new ImageIcon(chooseMaskImg()).getImage());
         } else {
             img = (new ImageIcon(chooseNoMaskImg()).getImage());
         }
@@ -49,9 +65,13 @@ public class Obstacle {
     }
 
     public String chooseNoMaskImg(){
-        int i = (int)(Math.random()*nomaskPaths.length);
-        return nomaskPaths[i];
+        int i = (int)(Math.random()*noMaskPaths.length);
+        return noMaskPaths[i];
     }
 
+    public String chooseMaskImg(){
+        int i = (int)(Math.random()*maskPaths.length);
+        return maskPaths[i];
+    }
 
 }
